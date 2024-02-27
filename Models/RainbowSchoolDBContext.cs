a6e3f8e68e04fe85ab2f1e2c8542ba427dccbdab
HP <HP@DESKTOP-AQ07QGH>
2024-02-27 13:31:40 +0530

    Deploy on Azure

---
+++ Models/RainbowSchoolDBContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace RainbowSchoolUI.Models
+{
+    public partial class RainbowSchoolDBContext : DbContext
+    {
+        public RainbowSchoolDBContext()
+        {
+        }
+
+        public RainbowSchoolDBContext(DbContextOptions<RainbowSchoolDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+        public virtual DbSet<Teacher> Teachers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:syedaserver.database.windows.net,1433;Initial Catalog=RainbowSchoolDB;Persist Security Info=False;User ID=syedaabidi;Password=syeda786Abidi;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+To deploy the application on Azure by creating a VM which will host and run the application on Azure cloud.
+
+Used the following:
+Visual Studio Windows: An IDE to code the application
+SQL Server 17 Express Edition: To develop desktop and web applications
+Windows Azure: Platform to run the applications on Azure cloud
